4ed0dde34b99758dba2d8abee2c1deadc6c19732
quoc <huyquoc2xx4@gmail.com>
2025-02-23 21:42:34 +0700

    updateupdate

---
+++ data.js
@@ -0,0 +1,29 @@
+const savedWishes = {
+    wishes: [
+        {
+            name: "Hồng Hoa A1",
+            message: "Cô dâu xinh quá! Chú rể cũng là người tinh tế, tinh cảm nữa nên tớ tin 2 bạn sẽ hạnh phúc bên nhau. Chúc cho cuộc sống sau này của 2 bạn dù có cùng nhau trải qua hy nô đi ở vẫn luôn yêu",
+            timestamp: "2024-03-08T10:10:00Z"
+        },
+        {
+            name: "Nguyễn Mạnh Dũng",
+            message: "Chúc mừng hạnh phúc",
+            timestamp: "2024-03-08T10:15:00Z"
+        },
+        {
+            name: "3123",
+            message: "321323213dsadsa",
+            timestamp: "2024-03-08T10:20:00Z"
+        },
+        {
+            name: "321321",
+            message: "Chúc mừng hạnh phúc! Chúc hai bạn trăm năm hạnh phúc!",
+            timestamp: "2024-03-08T10:25:00Z"
+        },
+        {
+            name: "minh",
+            message: "hellytgfjujkm",
+            timestamp: "2024-03-08T10:30:00Z"
+        }
+    ]
+}; 
